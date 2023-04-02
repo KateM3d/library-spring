c718e07b806c0915d5871506c4b092f155c9dd88
KateM3d <ekaterina8medvedeva@gmail.com>
2023-04-01 20:32:15 -0400

    add authors endpoint

--- src/main/java/com/libraryspring/libraryproject/controller/AuthorController.java
+++ src/main/java/com/libraryspring/libraryproject/controller/AuthorController.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class AuthorController {
 
     private final AuthorService authorService;
 
+    @GetMapping("/authors")
+    public List<AuthorDto> getAllAuthors() {
+        return authorService.getAllAuthors();
+    }
     @GetMapping("/author/{id}")
     AuthorDto getAuthorById(@PathVariable("id") Long id){
         return authorService.getAuthorById(id);
--- src/main/java/com/libraryspring/libraryproject/service/AuthorServiceImpl.java
+++ src/main/java/com/libraryspring/libraryproject/service/AuthorServiceImpl.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +23,28 @@ public class AuthorServiceImpl implements AuthorService {
         AuthorDto authorDto = convertEntityToDto(author);
         return authorDto;
 
+    }
+
+    @Override
+    public List<AuthorDto> getAllAuthors() {
+        List<Author> authors = authorRepository.findAll();
+        return authors.stream()
+                .map(author -> AuthorDto.builder()
+                        .id(author.getId())
+                        .name(author.getName())
+                        .surname(author.getSurname())
+                        .books(author.getBooks()
+                                .stream()
+                                .map(book -> BookDto.builder()
+                                        .id(book.getId())
+                                        .name(book.getName())
+                                        .genre(book.getGenre()
+                                                .getName())
+                                        .build())
+                                .collect(Collectors.toList()))
+
+                        .build())
+                .collect(Collectors.toList());
     }
 
     private AuthorDto convertEntityToDto(Author author) {
