f62a2024ab8cbc2b524170585d56689e88f960ae
KateM3d <ekaterina8medvedeva@gmail.com>
2023-04-20 21:11:30 -0400

    add security

--- src/main/java/com/libraryspring/libraryproject/config/SecurityConfig.java
+++ src/main/java/com/libraryspring/libraryproject/config/SecurityConfig.java
@@ -1,35 +1,52 @@
 package com.libraryspring.libraryproject.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorize) -> authorize.requestMatchers("/book")
-                        .hasRole("USER")
-                        .requestMatchers("/book/v2")
-                        .hasRole("ADMIN")
-                        .anyRequest()
-                        .authenticated())
+        http
+                .csrf().disable()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/users/").hasRole("USER")
+                        .requestMatchers("/authors/").hasRole("USER")
+                        .requestMatchers("/books/").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST,"/author/create").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT,"/author/update").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE,"/author/delete/").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST,"/book/create").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT,"/book/update").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE,"/book/delete/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .httpBasic();
 
         return http.build();
     }
 
     @Bean
-    public UserDetailsService users() {
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService users(PasswordEncoder passwordEncoder) {
+        User.UserBuilder users = User.builder().passwordEncoder(passwordEncoder::encode);
         UserDetails user = users.username("user")
                 .password("password1")
                 .roles("USER")
@@ -40,4 +57,5 @@ public class SecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
     }
+
 }
